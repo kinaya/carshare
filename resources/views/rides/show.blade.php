33ef88e3603b9b65d415d4f1e69f8e05fc2dca15
Alvine <alvine@tamkin.se>
2020-08-18 14:02:49 +0200

    small ui change

--- resources/views/rides/show.blade.php
+++ resources/views/rides/show.blade.php
@@ -3,13 +3,13 @@
 @section('content')
   <h1>{{$ride->title}}</h1>
   <p>Date: {{$ride->date}}</p>
-  <p>Distance: {{$ride->distance}}</p>
-  <p>Tax: {{$ride->tax}}</p>
-  <p>Amount: {{$ride->amount}}</p>
+  <p>Distance: {{$ride->distance}} km</p>
+  <p>Tax: {{$ride->tax}} kr</p>
+  <p>Amount: {{$ride->amount}} kr</p>
 
   @if(!Auth::guest())
     @if(Auth::user()->id == $ride->user_id)
-      <a href="/rides/{{$ride->id}}/edit" class="btn btn-default">Edit</a>
+      <a href="/rides/{{$ride->id}}/edit" class="btn btn-primary float-left mr-2">Edit</a>
       {!!Form::open(['action' => ['RidesController@destroy', $ride->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
       {{Form::hidden('_method', 'DELETE')}}
       {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
